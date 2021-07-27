313815b8ba2cd595e4423eda49c1c00fec68a176
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2021-07-27 23:09:54 +0200

    Add project files.

---
+++ RolePlayingGame/Controllers/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RolePlayingGame.Models;
+namespace RolePlayingGame.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private static Character knight = new Character();
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(knight);
+        }
+    }
+}
